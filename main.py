4cd009f3af4cfe6b6166384b0dbb86a17def55a2
༺ ｢ 𝐓𝐚𝐫𝐢𝐤 ｣ ༻ <159260646+tarikxvoid@users.noreply.github.com>
2025-05-04 18:37:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ intents.message_content = True
 intents.voice_states = True
 intents.guilds = True
 
-bot = commands.Bot(command_prefix=',', intents=intents)
+bot = commands.Bot(command_prefix='?', intents=intents)
 
 # Load bot token from Replit secrets
 TOKEN = os.getenv("TOKEN")
